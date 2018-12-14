560c2083c5eddb31687dcbc2071052d60f34dbc1
Raghab <ragshas@gmail.com>
2018-12-14 22:25:46 +0545

    connecting the backend and frontend, project complete

--- frontend.py
+++ frontend.py
@@ -1,7 +1,45 @@
 from tkinter import *
 import backend
 
+def get_selected_row(event):
+    try:
+        global selected_tuple
+        index=listbox1.curselection()[0]
+        selected_tuple=listbox1.get(index)
+        e_title.delete(0,END)
+        e_title.insert(END,selected_tuple[1])
+        e_author.delete(0,END)
+        e_author.insert(END,selected_tuple[2])
+        e_year.delete(0,END)
+        e_year.insert(END,selected_tuple[3])
+        e_isbn.delete(0,END)
+        e_isbn.insert(END,selected_tuple[4])
+    except IndexError:
+        pass
+
+def view_command():
+    listbox1.delete(0,END)
+    for row in backend.view():
+        listbox1.insert(END,row)
+
+def search_command():
+    listbox1.delete(0,END)
+    for row in backend.search(title_value.get(),author_value.get(),year_value.get(),isbn_value.get()):
+        listbox1.insert(END,row)
+
+def add_command():
+    backend.insert(title_value.get(),author_value.get(),year_value.get(),isbn_value.get())
+    listbox1.delete(0,END)
+    listbox1.insert(END,(title_value.get(),author_value.get(),year_value.get(),isbn_value.get()))
+
+def delete_command():
+    backend.delete(selected_tuple[0])
+
+def update_command():
+    backend.update(selected_tuple[0],title_value.get(),author_value.get(),year_value.get(),isbn_value.get())
+
 window = Tk()
+window.wm_title("Book Store")
 
 ltitle=Label(window, text="Title")
 ltitle.grid(row=0,column=0)
@@ -40,22 +78,24 @@ sb1.grid(row=2, column=2, rowspan=6)
 listbox1.configure(yscrollcommand=sb1.set)
 sb1.configure(command=listbox1.yview)
 
-b1=Button(window,text="View all", width=12)
+listbox1.bind('<<ListboxSelect>>', get_selected_row)
+
+b1=Button(window,text="View all", width=12, command=view_command)
 b1.grid(row=2, column=3)
 
-b1=Button(window,text="Search entry", width=12)
+b1=Button(window,text="Search entry", width=12, command=search_command)
 b1.grid(row=3, column=3)
 
-b1=Button(window,text="Add entry", width=12)
+b1=Button(window,text="Add entry", width=12, command=add_command)
 b1.grid(row=4, column=3)
 
-b1=Button(window,text="Update", width=12)
+b1=Button(window,text="Update", width=12, command=update_command)
 b1.grid(row=5, column=3)
 
-b1=Button(window,text="Delete", width=12)
+b1=Button(window,text="Delete", width=12, command=delete_command)
 b1.grid(row=6, column=3)
 
-b1=Button(window,text="Close", width=12)
+b1=Button(window,text="Close", width=12, command=window.destroy)
 b1.grid(row=7, column=3)
 
 
